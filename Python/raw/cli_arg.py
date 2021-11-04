37a01ff95985406249564522743733942eeffe49
dino <mvpstars@gmail.com>
2021-11-04 15:43:05 +0800

    Add Parsing Command Line Arguments

---
+++ Python/raw/cli_arg.py
@@ -0,0 +1,35 @@
+import time
+import sys
+
+StartTime = time.time()
+
+print ('Number of arguments:', len(sys.argv), 'arguments.')
+print ('Argument List:', str(sys.argv))
+print ('Argument List:', str(sys.argv[0]))
+
+import sys, getopt
+
+def main(argv):
+   inputfile = ''
+   outputfile = ''
+   try:
+      opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+   except getopt.GetoptError:
+      print ('test.py -i <inputfile> -o <outputfile>')
+      sys.exit(2)
+   for opt, arg in opts:
+      if opt == '-h':
+         print ('test.py -i <inputfile> -o <outputfile>')
+         sys.exit()
+      elif opt in ("-i", "--ifile"):
+         inputfile = arg
+      elif opt in ("-o", "--ofile"):
+         outputfile = arg
+   print ('Input file is "', inputfile)
+   print ('Output file is "', outputfile)
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
+
+EndTime = time.time()
+print("Durning Time(sec): ", EndTime - StartTime)
