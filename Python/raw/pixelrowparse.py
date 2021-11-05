a280e792d07a40e22a0a4d8b513d0c677d8bb921
dino <mvpstars@gmail.com>
2021-11-05 09:35:01 +0800

    Add one row parser

--- Python/raw/pixelrowparse.py
+++ Python/raw/pixelrowparse.py
@@ -10,15 +10,15 @@ StartTime = time.time()
 nWidth = 8000
 nHeight = 6000
 
-nFileCount = 20
+nFileCount = 100
 sFilePath = '/home/dino/RawShared/DtSample80006000Raw002/'
 sFileTempTime = '20211104112051'
 sFileTempFormat = 'P10'
 
-nROI_X = 3900
-nROI_Y = 2900
-nROI_W = 200
-nROI_H = 200
+nROI_X = 0
+nROI_Y = 0
+nROI_W = 8000
+nROI_H = 1
 
 sSavePath = '/home/dino/RawShared/Output/'
 #######################################################
@@ -28,7 +28,7 @@ sFileTempName = 'FrameID0_W{0:d}_H{1:d}_{2:s}_{3:s}_{4:04d}.raw'
 sSaveStdFile = 'test_std_{}.csv'
 sSaveAvgFile = 'test_avg_{}.csv'
 
-Pixel_array = np.zeros(nFileCount)
+PixelRow_array = np.zeros((nFileCount, nROI_W))
 lCsvStdRow = []
 lCsvAvgRow = []
 
@@ -47,28 +47,27 @@ def Save_CSV(FileName, RowInfo):
         # write a row to the csv file
         csv_writer.writerow(RowInfo)
 
-def Cal_Information(x, y, PixelArray):
-    Pixel_STD = np.std(PixelArray)
-    Pixel_AVG = np.average(PixelArray)
-    print('Pixel [{},{}]: STD:{} AVG:{}'.format(x, y, Pixel_STD, Pixel_AVG))
+def Cal_Information(y, PixelRowArray):
+    Pixel_STD = np.std(PixelRowArray, 1)
+    Pixel_AVG = np.average(PixelRowArray, 1)
+    #print('Pixel [{}]: STD:{} AVG:{}'.format(y, Pixel_STD, Pixel_AVG))
     lCsvStdRow.append(Pixel_STD)
     lCsvAvgRow.append(Pixel_AVG)
 
 for i in range(nROI_Y, nROI_Y+nROI_H):
     lCsvStdRow.clear()
     lCsvAvgRow.clear()
-    for j in range(nROI_X, nROI_X+nROI_W):
-        for k in range(0, nFileCount):
-            sFileTemp = sFilePath+sFileTempName.format(nWidth, nHeight, sFileTempTime, sFileTempFormat, k)
-            #print('File: ' + sFileTemp)
-            nPixelOffset = nWidth * i + j * 2
-            #print(nPixelOffset)
-            input_file = open(sFileTemp, 'rb')
-            input_array = np.fromfile(input_file, dtype=np.uint16, count=1, sep="", offset=nPixelOffset)
-            input_file.close()
-            Pixel_array[k] = input_array[0]
-            #print(Pixel_array[k])
-        Cal_Information(j, i, Pixel_array)
+    for k in range(0, nFileCount):
+        sFileTemp = sFilePath+sFileTempName.format(nWidth, nHeight, sFileTempTime, sFileTempFormat, k)
+        #print('File: ' + sFileTemp)
+        nPixelOffset = nWidth * i + nROI_X * 2
+        #print(nPixelOffset)
+        input_file = open(sFileTemp, 'rb')
+        input_array = np.fromfile(input_file, dtype=np.uint16, count=nROI_W, sep="", offset=nPixelOffset)
+        input_file.close()
+        PixelRow_array[k] = input_array
+        #print(PixelRow_array[k])
+    Cal_Information(i, PixelRow_array)
     Save_CSV(sSaveStdFile, lCsvStdRow)
     Save_CSV(sSaveAvgFile, lCsvAvgRow)
     RowTime = time.time()
