a9b4896b14abef24de4bcb799c6a87ddf711dfd7
dino <mvpstars@gmail.com>
2021-11-05 11:25:55 +0800

    Add split 4 color cal

--- Python/raw/pixelrowparse.py
+++ Python/raw/pixelrowparse.py
@@ -25,6 +25,12 @@ nROI_W = 200
 nROI_H = 200
 
 sSavePath = '/home/dino/RawShared/Output/'
+
+bAllPixel = True
+bOnlyRPixel = False
+bOnlyGrPixel = False
+bOnlyGbPixel = False
+bOnlyBPixel = False
 #######################################################
 
 # Normal
@@ -55,35 +61,95 @@ def Save_CSV(FileName, RowInfo):
         csv_writer.writerow(RowInfo)
 
 def Cal_Information(y, PixelRowArray):
+    #print(PixelRowArray)
     Pixel_STD = np.std(PixelRowArray, 0)
     Pixel_AVG = np.average(PixelRowArray, 0)
     #print('Pixel [{}]: STD:{} AVG:{}'.format(y, Pixel_STD, Pixel_AVG))
     #lCsvStdRow.append(Pixel_STD)
     #lCsvAvgRow.append(Pixel_AVG)
     lCsvStdRow.extend(Pixel_STD.tolist())
-    #print(lCsvStdRow)
     lCsvAvgRow.extend(Pixel_AVG.tolist())
+    #print(lCsvStdRow)
     #print(lCsvAvgRow)
 
-for i in range(nROI_Y, nROI_Y+nROI_H):
-    lCsvStdRow.clear()
-    lCsvAvgRow.clear()
-    for k in range(0, nFileCount):
-        #sFileTemp = sFilePath+sFileTempName.format(nWidth, nHeight, sFileTempTime, sFileTempFormat, k)
-        sFileTemp = sFilePath+sFileTempName.format(nWidth, nHeight, sFileTempTime, sFileTempFormat, k+((nFileExposureIM-1)*nFileExposureCount), nFileExposureIM, nFileExposureID)
-        #print('File: ' + sFileTemp)
-        nPixelOffset = nWidth * i + nROI_X * 2
-        #print(nPixelOffset)
-        input_file = open(sFileTemp, 'rb')
-        input_array = np.fromfile(input_file, dtype=np.uint16, count=nROI_W, sep="", offset=nPixelOffset)
-        input_file.close()
-        PixelRow_array[k] = input_array
-        #print(PixelRow_array[k])
-    Cal_Information(i, PixelRow_array)
-    Save_CSV(sSaveStdFile, lCsvStdRow)
-    Save_CSV(sSaveAvgFile, lCsvAvgRow)
-    RowTime = time.time()
-    print("Durning Row Time(sec): ", RowTime - StartTime)
+def ParsingPixel():
+    for i in range(nROI_Y, nROI_Y+nROI_H):
+        lCsvStdRow.clear()
+        lCsvAvgRow.clear()
+        bNeedCal = False
+        if bAllPixel:
+            # do nothing
+            #pass
+            #None
+            PixelRow_array = np.zeros((nFileCount, nROI_W))
+        elif bOnlyRPixel and (i%4!=0 and i%4!=1):
+            continue
+        elif bOnlyGrPixel and (i%4!=0 and i%4!=1):
+            continue
+        elif bOnlyGbPixel and (i%4!=2 and i%4!=3):
+            continue
+        elif bOnlyBPixel and (i%4!=2 and i%4!=3):
+            continue
+        else:
+            PixelRow_array = np.zeros((nFileCount, nROI_W//2))
+
+        for k in range(0, nFileCount):
+            bNeedCal = True
+            #sFileTemp = sFilePath+sFileTempName.format(nWidth, nHeight, sFileTempTime, sFileTempFormat, k)
+            sFileTemp = sFilePath+sFileTempName.format(nWidth, nHeight, sFileTempTime, sFileTempFormat, k+((nFileExposureIM-1)*nFileExposureCount), nFileExposureIM, nFileExposureID)
+            #print('File: ' + sFileTemp)
+            nPixelOffset = nWidth * i + nROI_X * 2
+            #print(nPixelOffset)
+            input_file = open(sFileTemp, 'rb')
+            input_array = np.fromfile(input_file, dtype=np.uint16, count=nROI_W, sep="", offset=nPixelOffset)
+            input_file.close()
+            
+            RemoveList = []
+            if bAllPixel:
+                # do nothing
+                #pass
+                #None
+                PixelRow_array[k] = input_array
+            elif bOnlyRPixel:
+                if (i%4==0 or i%4==1):
+                    for l in range(0, nROI_W):
+                        nIndex = nROI_X + l
+                        if (nIndex%4==2 or nIndex%4==3):   #Gr
+                            RemoveList.append(l)
+                    PixelRow_array[k] = np.delete(input_array, RemoveList)
+            elif bOnlyGrPixel:
+                if (i%4==0 or i%4==1):
+                    for l in range(0, nROI_W):
+                        nIndex = nROI_X + l
+                        if (nIndex%4==0 or nIndex%4==1):   #R
+                            RemoveList.append(l)
+                    PixelRow_array[k] = np.delete(input_array, RemoveList)
+            elif bOnlyGbPixel:
+                if (i%4==2 or i%4==3):
+                    for l in range(0, nROI_W):
+                        nIndex = nROI_X + l
+                        if (nIndex%4==2 or nIndex%4==3):   #B
+                            RemoveList.append(l)
+                    PixelRow_array[k] = np.delete(input_array, RemoveList)
+            elif bOnlyBPixel:
+                if (i%4==2 or i%4==3):
+                    for l in range(0, nROI_W):
+                        nIndex = nROI_X + l
+                        if (nIndex%4==0 or nIndex%4==1):   #Gb
+                            RemoveList.append(l)
+                    PixelRow_array[k] = np.delete(input_array, RemoveList)
+            #print(PixelRow_array[k])
+
+        if bNeedCal:
+            #print(PixelRow_array)
+            Cal_Information(i, PixelRow_array)
+            Save_CSV(sSaveStdFile, lCsvStdRow)
+            Save_CSV(sSaveAvgFile, lCsvAvgRow)
+            RowTime = time.time()
+            print("Durning Row Time(sec): ", RowTime - StartTime)
+
+if __name__ == "__main__":
+    ParsingPixel()
 
 EndTime = time.time()
 print("Durning Time(sec): ", EndTime - StartTime)
