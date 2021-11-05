c95329e0b1b0bfed96cddcc8d6ab2747f8adcafb
dino <mvpstars@gmail.com>
2021-11-05 13:36:28 +0800

    Add multiple interval exposure time

--- Python/raw/pixelrowparse.py
+++ Python/raw/pixelrowparse.py
@@ -34,6 +34,7 @@ sFileTempFormat = 'P10'
 nFileExposureIM = 1
 nFileExposureID = 30
 nFileExposureCount = 10
+nFileExposureInterval = 9
 
 nROI_X = 3900
 nROI_Y = 2900
@@ -49,6 +50,7 @@ nPixelSelect = PixelSelect.AutoSplit
 if not bExposureRaw:
     # Normal
     sFileTempName = 'FrameID0_W{0:d}_H{1:d}_{2:s}_{3:s}_{4:04d}.raw'
+    nFileExposureInterval = 1
 else:
     # Exposure
     sFileTempName = 'FrameID0_W{0:d}_H{1:d}_{2:s}_{3:s}_{4:04d}_{5:d}_{6:d}.raw'
@@ -90,152 +92,155 @@ def Cal_Information(y, PixelRowArray):
     #print(lCsvAvgRow)
 
 def ParsingPixel():
-    if nPixelSelect != PixelSelect.AutoSplit:
-        sSaveOneStdFile = sSavePath+sSaveStdFile.format(TimeInfo)
-        sSaveOneAvgFile = sSavePath+sSaveAvgFile.format(TimeInfo)
-        #print(sSaveOneStdFile)
-        #print(sSaveOneAvgFile)
-    else:
-        sSaveRStdFile = sSavePath+sSaveStdFile.format(TimeInfo, nFileExposureIM, nFileExposureID, 'R')
-        sSaveRAvgFile = sSavePath+sSaveAvgFile.format(TimeInfo, nFileExposureIM, nFileExposureID, 'R')
-        sSaveGrStdFile = sSavePath+sSaveStdFile.format(TimeInfo, nFileExposureIM, nFileExposureID, 'Gr')
-        sSaveGrAvgFile = sSavePath+sSaveAvgFile.format(TimeInfo, nFileExposureIM, nFileExposureID, 'Gr')
-        sSaveGbStdFile = sSavePath+sSaveStdFile.format(TimeInfo, nFileExposureIM, nFileExposureID, 'Gb')
-        sSaveGbAvgFile = sSavePath+sSaveAvgFile.format(TimeInfo, nFileExposureIM, nFileExposureID, 'Gb')
-        sSaveBStdFile = sSavePath+sSaveStdFile.format(TimeInfo, nFileExposureIM, nFileExposureID, 'B')
-        sSaveBAvgFile = sSavePath+sSaveAvgFile.format(TimeInfo, nFileExposureIM, nFileExposureID, 'B')
-
-    for i in range(nROI_Y, nROI_Y+nROI_H):
-        bNeedCal = False
-
-        if nPixelSelect == PixelSelect.AllPixel:
-            # do nothing
-            #pass
-            #None
-            PixelRow_array = np.zeros((nFileCount, nROI_W))
-        elif nPixelSelect == PixelSelect.AutoSplit:
-            PixelRowR_array = np.zeros((nFileCount, nROI_W//2))
-            PixelRowGr_array = np.zeros((nFileCount, nROI_W//2))
-            PixelRowGb_array = np.zeros((nFileCount, nROI_W//2))
-            PixelRowB_array = np.zeros((nFileCount, nROI_W//2))
-        elif (nPixelSelect == PixelSelect.OnlyRPixel) and (i%4!=0 and i%4!=1):
-            continue
-        elif (nPixelSelect == PixelSelect.OnlyGrPixel) and (i%4!=0 and i%4!=1):
-            continue
-        elif (nPixelSelect == PixelSelect.OnlyGbPixel) and (i%4!=2 and i%4!=3):
-            continue
-        elif (nPixelSelect == PixelSelect.OnlyBPixel) and (i%4!=2 and i%4!=3):
-            continue
+    for h in range(0, nFileExposureInterval):
+        if nPixelSelect != PixelSelect.AutoSplit:
+            sSaveOneStdFile = sSavePath+sSaveStdFile.format(TimeInfo)
+            sSaveOneAvgFile = sSavePath+sSaveAvgFile.format(TimeInfo)
+            #print(sSaveOneStdFile)
+            #print(sSaveOneAvgFile)
         else:
-            PixelRow_array = np.zeros((nFileCount, nROI_W//2))
+            sSaveRStdFile = sSavePath+sSaveStdFile.format(TimeInfo, h+nFileExposureIM, nFileExposureID, 'R')
+            sSaveRAvgFile = sSavePath+sSaveAvgFile.format(TimeInfo, h+nFileExposureIM, nFileExposureID, 'R')
+            sSaveGrStdFile = sSavePath+sSaveStdFile.format(TimeInfo, h+nFileExposureIM, nFileExposureID, 'Gr')
+            sSaveGrAvgFile = sSavePath+sSaveAvgFile.format(TimeInfo, h+nFileExposureIM, nFileExposureID, 'Gr')
+            sSaveGbStdFile = sSavePath+sSaveStdFile.format(TimeInfo, h+nFileExposureIM, nFileExposureID, 'Gb')
+            sSaveGbAvgFile = sSavePath+sSaveAvgFile.format(TimeInfo, h+nFileExposureIM, nFileExposureID, 'Gb')
+            sSaveBStdFile = sSavePath+sSaveStdFile.format(TimeInfo, h+nFileExposureIM, nFileExposureID, 'B')
+            sSaveBAvgFile = sSavePath+sSaveAvgFile.format(TimeInfo, h+nFileExposureIM, nFileExposureID, 'B')
 
-        bNeedCal = True
-        bR_Gr = False
-        bGb_B = False
-        for k in range(0, nFileCount):
-            #sFileTemp = sFilePath+sFileTempName.format(nWidth, nHeight, sFileTempTime, sFileTempFormat, k)
-            sFileTemp = sFilePath+sFileTempName.format(nWidth, nHeight, sFileTempTime, sFileTempFormat, k+((nFileExposureIM-1)*nFileExposureCount), nFileExposureIM, nFileExposureID)
-            #print('File: ' + sFileTemp)
-            nPixelOffset = nWidth * i + nROI_X * 2
-            #print(nPixelOffset)
-            input_file = open(sFileTemp, 'rb')
-            input_array = np.fromfile(input_file, dtype=np.uint16, count=nROI_W, sep="", offset=nPixelOffset)
-            input_file.close()
-            
-            RemoveList = []
-            RemoveList2 = []
+        for i in range(nROI_Y, nROI_Y+nROI_H):
+            bNeedCal = False
+
             if nPixelSelect == PixelSelect.AllPixel:
                 # do nothing
                 #pass
                 #None
-                PixelRow_array[k] = input_array
+                PixelRow_array = np.zeros((nFileCount, nROI_W))
             elif nPixelSelect == PixelSelect.AutoSplit:
-                if (i%4==0 or i%4==1):  #R+Gr
-                    for l in range(0, nROI_W):
-                        nIndex = nROI_X + l
-                        if (nIndex%4==0 or nIndex%4==1):   #R
-                            RemoveList.append(l)
-                        elif (nIndex%4==2 or nIndex%4==3):   #Gr
-                            RemoveList2.append(l)
-                    PixelRowR_array[k] = np.delete(input_array, RemoveList2)
-                    PixelRowGr_array[k] = np.delete(input_array, RemoveList)
-                    bR_Gr = True
-                elif (i%4==2 or i%4==3):    #Gr+B
-                    for l in range(0, nROI_W):
-                        nIndex = nROI_X + l
-                        if (nIndex%4==0 or nIndex%4==1):   #Gb
-                            RemoveList.append(l)
-                        elif (nIndex%4==2 or nIndex%4==3):   #B
-                            RemoveList2.append(l)
-                    PixelRowGb_array[k] = np.delete(input_array, RemoveList2)
-                    PixelRowB_array[k] = np.delete(input_array, RemoveList)
-                    bGb_B = True
-            elif nPixelSelect == PixelSelect.OnlyRPixel:
-                if (i%4==0 or i%4==1):
-                    for l in range(0, nROI_W):
-                        nIndex = nROI_X + l
-                        if (nIndex%4==2 or nIndex%4==3):   #Gr
-                            RemoveList.append(l)
-                    PixelRow_array[k] = np.delete(input_array, RemoveList)
-            elif nPixelSelect == PixelSelect.OnlyGrPixel:
-                if (i%4==0 or i%4==1):
-                    for l in range(0, nROI_W):
-                        nIndex = nROI_X + l
-                        if (nIndex%4==0 or nIndex%4==1):   #R
-                            RemoveList.append(l)
-                    PixelRow_array[k] = np.delete(input_array, RemoveList)
-            elif nPixelSelect == PixelSelect.OnlyGbPixel:
-                if (i%4==2 or i%4==3):
-                    for l in range(0, nROI_W):
-                        nIndex = nROI_X + l
-                        if (nIndex%4==2 or nIndex%4==3):   #B
-                            RemoveList.append(l)
-                    PixelRow_array[k] = np.delete(input_array, RemoveList)
-            elif nPixelSelect == PixelSelect.OnlyBPixel:
-                if (i%4==2 or i%4==3):
-                    for l in range(0, nROI_W):
-                        nIndex = nROI_X + l
-                        if (nIndex%4==0 or nIndex%4==1):   #Gb
-                            RemoveList.append(l)
-                    PixelRow_array[k] = np.delete(input_array, RemoveList)
-            #print(PixelRow_array[k])
-
-        if bNeedCal:
-            #print(PixelRow_array)
-            if nPixelSelect != PixelSelect.AutoSplit:
-                lCsvStdRow.clear()
-                lCsvAvgRow.clear()
-                Cal_Information(i, PixelRow_array)
-                Save_CSV(sSaveOneStdFile, lCsvStdRow)
-                Save_CSV(sSaveOneAvgFile, lCsvAvgRow)
+                PixelRowR_array = np.zeros((nFileCount, nROI_W//2))
+                PixelRowGr_array = np.zeros((nFileCount, nROI_W//2))
+                PixelRowGb_array = np.zeros((nFileCount, nROI_W//2))
+                PixelRowB_array = np.zeros((nFileCount, nROI_W//2))
+            elif (nPixelSelect == PixelSelect.OnlyRPixel) and (i%4!=0 and i%4!=1):
+                continue
+            elif (nPixelSelect == PixelSelect.OnlyGrPixel) and (i%4!=0 and i%4!=1):
+                continue
+            elif (nPixelSelect == PixelSelect.OnlyGbPixel) and (i%4!=2 and i%4!=3):
+                continue
+            elif (nPixelSelect == PixelSelect.OnlyBPixel) and (i%4!=2 and i%4!=3):
+                continue
             else:
-                if bR_Gr:
-                    lCsvStdRow.clear()
-                    lCsvAvgRow.clear()
-                    Cal_Information(i, PixelRowR_array)
-                    Save_CSV(sSaveRStdFile, lCsvStdRow)
-                    Save_CSV(sSaveRAvgFile, lCsvAvgRow)
+                PixelRow_array = np.zeros((nFileCount, nROI_W//2))
 
-                    lCsvStdRow.clear()
-                    lCsvAvgRow.clear()
-                    Cal_Information(i, PixelRowGr_array)
-                    Save_CSV(sSaveGrStdFile, lCsvStdRow)
-                    Save_CSV(sSaveGrAvgFile, lCsvAvgRow)
-                elif bGb_B:
-                    lCsvStdRow.clear()
-                    lCsvAvgRow.clear()
-                    Cal_Information(i, PixelRowGb_array)
-                    Save_CSV(sSaveGbStdFile, lCsvStdRow)
-                    Save_CSV(sSaveGbAvgFile, lCsvAvgRow)
-
-                    lCsvStdRow.clear()
-                    lCsvAvgRow.clear()
-                    Cal_Information(i, PixelRowB_array)
-                    Save_CSV(sSaveBStdFile, lCsvStdRow)
-                    Save_CSV(sSaveBAvgFile, lCsvAvgRow)
+            bNeedCal = True
+            bR_Gr = False
+            bGb_B = False
+            for k in range(0, nFileCount):
+                if not bExposureRaw:
+                    sFileTemp = sFilePath+sFileTempName.format(nWidth, nHeight, sFileTempTime, sFileTempFormat, k)
+                else:
+                    sFileTemp = sFilePath+sFileTempName.format(nWidth, nHeight, sFileTempTime, sFileTempFormat, k+((h+nFileExposureIM-1)*nFileExposureCount), h+nFileExposureIM, nFileExposureID)
+                #print('File: ' + sFileTemp)
+                nPixelOffset = nWidth * i + nROI_X * 2
+                #print(nPixelOffset)
+                input_file = open(sFileTemp, 'rb')
+                input_array = np.fromfile(input_file, dtype=np.uint16, count=nROI_W, sep="", offset=nPixelOffset)
+                input_file.close()
                 
-            RowTime = time.time()
-            print("Durning Row Time(sec): ", RowTime - StartTime)
+                RemoveList = []
+                RemoveList2 = []
+                if nPixelSelect == PixelSelect.AllPixel:
+                    # do nothing
+                    #pass
+                    #None
+                    PixelRow_array[k] = input_array
+                elif nPixelSelect == PixelSelect.AutoSplit:
+                    if (i%4==0 or i%4==1):  #R+Gr
+                        for l in range(0, nROI_W):
+                            nIndex = nROI_X + l
+                            if (nIndex%4==0 or nIndex%4==1):   #R
+                                RemoveList.append(l)
+                            elif (nIndex%4==2 or nIndex%4==3):   #Gr
+                                RemoveList2.append(l)
+                        PixelRowR_array[k] = np.delete(input_array, RemoveList2)
+                        PixelRowGr_array[k] = np.delete(input_array, RemoveList)
+                        bR_Gr = True
+                    elif (i%4==2 or i%4==3):    #Gr+B
+                        for l in range(0, nROI_W):
+                            nIndex = nROI_X + l
+                            if (nIndex%4==0 or nIndex%4==1):   #Gb
+                                RemoveList.append(l)
+                            elif (nIndex%4==2 or nIndex%4==3):   #B
+                                RemoveList2.append(l)
+                        PixelRowGb_array[k] = np.delete(input_array, RemoveList2)
+                        PixelRowB_array[k] = np.delete(input_array, RemoveList)
+                        bGb_B = True
+                elif nPixelSelect == PixelSelect.OnlyRPixel:
+                    if (i%4==0 or i%4==1):
+                        for l in range(0, nROI_W):
+                            nIndex = nROI_X + l
+                            if (nIndex%4==2 or nIndex%4==3):   #Gr
+                                RemoveList.append(l)
+                        PixelRow_array[k] = np.delete(input_array, RemoveList)
+                elif nPixelSelect == PixelSelect.OnlyGrPixel:
+                    if (i%4==0 or i%4==1):
+                        for l in range(0, nROI_W):
+                            nIndex = nROI_X + l
+                            if (nIndex%4==0 or nIndex%4==1):   #R
+                                RemoveList.append(l)
+                        PixelRow_array[k] = np.delete(input_array, RemoveList)
+                elif nPixelSelect == PixelSelect.OnlyGbPixel:
+                    if (i%4==2 or i%4==3):
+                        for l in range(0, nROI_W):
+                            nIndex = nROI_X + l
+                            if (nIndex%4==2 or nIndex%4==3):   #B
+                                RemoveList.append(l)
+                        PixelRow_array[k] = np.delete(input_array, RemoveList)
+                elif nPixelSelect == PixelSelect.OnlyBPixel:
+                    if (i%4==2 or i%4==3):
+                        for l in range(0, nROI_W):
+                            nIndex = nROI_X + l
+                            if (nIndex%4==0 or nIndex%4==1):   #Gb
+                                RemoveList.append(l)
+                        PixelRow_array[k] = np.delete(input_array, RemoveList)
+                #print(PixelRow_array[k])
+
+            if bNeedCal:
+                #print(PixelRow_array)
+                if nPixelSelect != PixelSelect.AutoSplit:
+                    lCsvStdRow.clear()
+                    lCsvAvgRow.clear()
+                    Cal_Information(i, PixelRow_array)
+                    Save_CSV(sSaveOneStdFile, lCsvStdRow)
+                    Save_CSV(sSaveOneAvgFile, lCsvAvgRow)
+                else:
+                    if bR_Gr:
+                        lCsvStdRow.clear()
+                        lCsvAvgRow.clear()
+                        Cal_Information(i, PixelRowR_array)
+                        Save_CSV(sSaveRStdFile, lCsvStdRow)
+                        Save_CSV(sSaveRAvgFile, lCsvAvgRow)
+
+                        lCsvStdRow.clear()
+                        lCsvAvgRow.clear()
+                        Cal_Information(i, PixelRowGr_array)
+                        Save_CSV(sSaveGrStdFile, lCsvStdRow)
+                        Save_CSV(sSaveGrAvgFile, lCsvAvgRow)
+                    elif bGb_B:
+                        lCsvStdRow.clear()
+                        lCsvAvgRow.clear()
+                        Cal_Information(i, PixelRowGb_array)
+                        Save_CSV(sSaveGbStdFile, lCsvStdRow)
+                        Save_CSV(sSaveGbAvgFile, lCsvAvgRow)
+
+                        lCsvStdRow.clear()
+                        lCsvAvgRow.clear()
+                        Cal_Information(i, PixelRowB_array)
+                        Save_CSV(sSaveBStdFile, lCsvStdRow)
+                        Save_CSV(sSaveBAvgFile, lCsvAvgRow)
+                    
+                RowTime = time.time()
+                print("Durning Row Time(sec): ", RowTime - StartTime)
 
 if __name__ == "__main__":
     ParsingPixel()
