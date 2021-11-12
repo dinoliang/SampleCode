68779d2af8137566f2642cd680f5f40533b49df0
dino <mvpstars@gmail.com>
2021-11-12 09:52:55 +0800

    Add some test

--- Python/raw/test.py
+++ Python/raw/test.py
@@ -51,17 +51,16 @@ def test2D_2():
     print('np.size(X, 0): ', np.size(X, 0))
     print('np.size(X, 1): ', np.size(X, 1))
     print('np.prod(X.shape): ', np.prod(X.shape))
+    X_dim = X.shape
+    print('X.shape[0]: ', X_dim[0])
+    print('X.shape[1]: ', X_dim[1])
+    print('len(X): ', len(X))
     print('X.sum(axis=0): ', X.sum(axis=0))
     print('X.sum(axis=1): ', X.sum(axis=1))
     print('np.std(X, 0): ', np.std(X, 0))
     print('np.std(X, 1): ', np.std(X, 1))
     print('np.std(X): ', np.std(X))
     print('X.T: \n', X.T)
-
-
-def testText():
-    print('Hello', end='')
-    print('World')
 
 
 def np_append_test():
@@ -88,6 +87,40 @@ def np_append_test():
     print(empty_array)
 
 
+def testText():
+    print('Hello', end='')
+    print('World')
+
+
+def numpy_condition_001():
+    x = np.array([[10, 20, 30],
+               [3, 50, 5]])
+    y = np.array([[70, 80, 90],
+                [100, 110, 120]])
+    condition = np.where(x>20,x,y)
+    print("Input array :")
+    print(x)
+    print(y)
+    print("Output array with condition applied:")
+    print(condition)
+
+
+def numpy_condition_002():
+    m = np.where([[True, False, True],
+                  [False, True, False]],
+                 [[1,2,3],
+                  [4, 5, 6]],
+                 [[7,8,9],
+                  [10, 11, 12]])
+    print(m)
+
+
+def numpy_condition_003():
+    m = np.array([1,2,3,4,5])
+    n = np.where((m > 1) & (m < 5), m, 0)
+    print(n)
+
+
 if __name__ == "__main__":
     '''
     testSTD_AVG()
@@ -104,6 +137,9 @@ if __name__ == "__main__":
     '''
     '''
     testText()
+    '''
+    '''
+    numpy_condition_003()
     '''
 
 
