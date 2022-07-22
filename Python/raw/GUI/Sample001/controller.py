53d75263e0375bed999377e224df2342c1bad9f8
dino <mvpstars@gmail.com>
2022-07-22 17:23:07 +0800

    Combine zett

--- Python/raw/GUI/Sample001/controller.py
+++ Python/raw/GUI/Sample001/controller.py
@@ -7,9 +7,17 @@ from PyQt5 import QtCore
 from ui_untitled import Ui_MainWindow
 import sys
 import numpy as np
+import csv
+
+sys.path.append('/home/dino/PythonShared/raw/')
+import channelrowparse_zett as zettmain
 
 
 class MainWindow(QtWidgets.QMainWindow):
+    input_Folder = ''
+    output_Folder = ''
+    condition_Array = ''
+
     def __init__(self):
         super(MainWindow, self).__init__()
         self.ui = Ui_MainWindow()
@@ -54,16 +62,37 @@ class MainWindow(QtWidgets.QMainWindow):
         textFileTimestamp = self.ui.lineEdit_FileTimestamp.text()
         print("File Timestamp: {}".format(textFileTimestamp))
 
+        print(self.input_Folder)
+        print(self.output_Folder)
+        print(self.condition_Array)
+
+        zettmain.CallMain(  nWidth=np.int64(int(textWidth, 10)), \
+                            nHeight=np.int64(int(textHeight, 10)), \
+                            nX=np.int64(int(textX, 10)), \
+                            nY=np.int64(int(textY, 10)), \
+                            nROI_W=np.int64(int(textROIWidth, 10)), \
+                            nROI_H=np.int64(int(textROIHeight, 10)), \
+                            nColIndex=np.int64(int(textColumnIndex, 10)), \
+                            nRowIndex=np.int64(int(textRowIndex, 10)), \
+                            nFileCounts=np.int64(int(textFileCounts, 10)), \
+                            FileTimeStamp=textFileTimestamp, \
+                            InputFolder=self.input_Folder, \
+                            OutputFolder=self.output_Folder, \
+                            ArrayFolder=self.condition_Array)
+        print(zettmain.g_sFilePathFolder)
+
         return
 
     def buttonClick_InputFolder(self):
-        input_Folder = QFileDialog.getExistingDirectory(self, 'Choose Input Folder', '/home/dino/IMX586_Bin/')
-        self.ui.lineEdit_InputFolder.setText(input_Folder)
+        self.input_Folder = QFileDialog.getExistingDirectory(self, 'Choose Input Folder', '/home/dino/IMX586_Bin/')
+        self.input_Folder = self.input_Folder + '/{}/'
+        self.ui.lineEdit_InputFolder.setText(self.input_Folder)
         return
 
     def buttonClick_OutputFolder(self):
-        output_Folder = QFileDialog.getExistingDirectory(self, 'Choose Output Folder', '/home/dino/RawShared/Output/')
-        self.ui.lineEdit_OutputFolder.setText(output_Folder)
+        self.output_Folder = QFileDialog.getExistingDirectory(self, 'Choose Output Folder', '/home/dino/RawShared/Output/')
+        self.output_Folder = self.output_Folder + '/{}/'
+        self.ui.lineEdit_OutputFolder.setText(self.output_Folder)
         return
 
     def buttonClick_Condition(self):
@@ -71,4 +100,14 @@ class MainWindow(QtWidgets.QMainWindow):
         options |= QFileDialog.DontUseNativeDialog
         CSV_fileName, _ = QFileDialog.getOpenFileName(self, 'Choose Condition CSV', '', 'CSV File (*.csv)', options=options)
         self.ui.lineEdit_Condition.setText(CSV_fileName)
+
+        with open(CSV_fileName, newline='') as csvfile:
+            # 讀取 CSV 檔案內容
+            rows = csv.reader(csvfile)
+
+            # 以迴圈輸出每一列
+            for row in rows:
+                self.condition_Array = row
+                print(self.condition_Array)
+
         return
