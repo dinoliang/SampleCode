dddadd1b09287ce43e5c68a085c2cd06e6b59767
dino <mvpstars@gmail.com>
2022-07-27 14:32:25 +0800

    Add more feature and protection

--- Python/raw/GUI/Sample001/controller.py
+++ Python/raw/GUI/Sample001/controller.py
@@ -8,6 +8,7 @@ from ui_untitled import Ui_MainWindow
 import sys
 import numpy as np
 import csv
+import os
 
 sys.path.append('/home/dino/PythonShared/raw/')
 import channelrowparse_zett as zettmain
@@ -40,6 +41,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.HideUIbyQE()
         return
 
+    # UI Display
     def ShowAllUI(self):
         self.ui.label_Width.setHidden(False)
         self.ui.lineEdit_Width.setHidden(False)
@@ -111,10 +113,18 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui.lineEdit_RowIndex_2.setHidden(True)
         return
 
+    # Botton Action
     def buttonClick_Start(self):
         if self.ui.lineEdit_Width.isVisible():
-            textWidth = self.ui.lineEdit_Width.text()
-            print("Image Width: {:d}".format(np.int64(int(textWidth, 10))))
+            try:
+                textWidth = self.ui.lineEdit_Width.text()
+                print("Image Width: {:d}".format(np.int64(int(textWidth, 10))))
+            except ValueError as error:
+                print(error)
+            else:
+                pass
+            finally:
+                pass
 
         if self.ui.lineEdit_Height.isVisible():
             textHeight = self.ui.lineEdit_Height.text()
@@ -223,31 +233,39 @@ class MainWindow(QtWidgets.QMainWindow):
         return
 
     def buttonClick_InputFolder(self):
-        self.input_Folder = QFileDialog.getExistingDirectory(self, 'Choose Input Folder', '/home/dino/IMX586_Bin/')
-        self.input_Folder = self.input_Folder + '/{}/'
-        self.ui.lineEdit_InputFolder.setText(self.input_Folder)
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        self.input_Folder = QFileDialog.getExistingDirectory(self, 'Choose Input Folder', '/home/dino/IMX586_Bin/', options=options)
+        if self.input_Folder != '':
+            self.input_Folder = self.input_Folder + '/{}/'
+            self.ui.lineEdit_InputFolder.setText(self.input_Folder)
         return
 
     def buttonClick_OutputFolder(self):
-        self.output_Folder = QFileDialog.getExistingDirectory(self, 'Choose Output Folder', '/home/dino/RawShared/Output/')
-        self.output_Folder = self.output_Folder + '/{}/'
-        self.ui.lineEdit_OutputFolder.setText(self.output_Folder)
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        self.output_Folder = QFileDialog.getExistingDirectory(self, 'Choose Output Folder', '/home/dino/RawShared/Output/', options=options)
+        if self.output_Folder != '':
+            self.output_Folder = self.output_Folder + '/{}/'
+            self.ui.lineEdit_OutputFolder.setText(self.output_Folder)
+
+        #self.CreateFolder()
         return
 
     def buttonClick_Condition(self):
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
         CSV_fileName, _ = QFileDialog.getOpenFileName(self, 'Choose Condition CSV', '', 'CSV File (*.csv)', options=options)
-        self.ui.lineEdit_Condition.setText(CSV_fileName)
+        if CSV_fileName != '':
+            self.ui.lineEdit_Condition.setText(CSV_fileName)
+            with open(CSV_fileName, newline='') as csvfile:
+                # 讀取 CSV 檔案內容
+                rows = csv.reader(csvfile)
 
-        with open(CSV_fileName, newline='') as csvfile:
-            # 讀取 CSV 檔案內容
-            rows = csv.reader(csvfile)
-
-            # 以迴圈輸出每一列
-            for row in rows:
-                self.condition_Array = row
-                print(self.condition_Array)
+                # 以迴圈輸出每一列
+                for row in rows:
+                    self.condition_Array = row
+                    print(self.condition_Array)
 
         return
 
@@ -262,8 +280,43 @@ class MainWindow(QtWidgets.QMainWindow):
             self.HideUIbyPixel()
         return
 
+    # Callback
     def Message_Callback(self, strMessage):
         print(strMessage)
         strShowMsg = 'Msg: ' + strMessage
         self.ui.lineEdit_Msg.setText(strShowMsg)
+        return
+
+    # Other function
+    def CreateFolder(self):
+        if self.output_Folder == '' or np.size(self.condition_Array) <= 0:
+            return
+
+        for foldername in self.condition_Array:
+            #folder = os.path.join(self.output_Folder, foldername)
+            folder = self.output_Folder.format(foldername)
+            if os.path.exists(folder):
+                continue
+            try:
+                os.mkdir(folder)
+            except OSError as error:
+                print(error)
+            else:
+                pass
+            finally:
+                pass
+
+        #folder = os.path.join(self.output_Folder, 'Output')
+        folder = self.output_Folder.format('Output')
+        if os.path.exists(folder):
+            return
+        try:
+            os.mkdir(folder)
+        except OSError as error:
+            print(error)
+        else:
+            pass
+        finally:
+            pass
+
         return
