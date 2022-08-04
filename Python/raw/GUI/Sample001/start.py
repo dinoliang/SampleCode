87759961c6e040ba51a950a70616aaacecbc2e64
dino <mvpstars@gmail.com>
2022-08-04 16:17:47 +0800

    Update some comment

--- Python/raw/GUI/Sample001/start.py
+++ Python/raw/GUI/Sample001/start.py
@@ -1,10 +1,11 @@
 # UI Command: pyuic5 -x untitled.ui -o ui_untitled.py
-# Installer Command: pyinstaller -F ui_untitled.py
+# Installer Command: pyinstaller -F start.py
 
 from PyQt5 import QtWidgets
 from controller import MainWindow
 import sys
 
+# import in advance for pyinstaller package
 from PyQt5.QtWidgets import QFileDialog
 import numpy as np
 import csv
