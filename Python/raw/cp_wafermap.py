a7a3470219cf3024b6bb35574d2d3675215d71ce
dino <mvpstars@gmail.com>
2022-08-04 17:49:48 +0800

    Update check info of file name

--- Python/raw/cp_wafermap.py
+++ Python/raw/cp_wafermap.py
@@ -35,7 +35,7 @@ class ResultHandler:
             for sFile in files:
                 if '.csv' not in sFile:
                     continue
-                if 'Result_' in sFile:
+                if 'Result' in sFile or 'Out' in sFile:
                     continue
                 filepathe = os.path.join(resultFolder, sFile)
                 csvfile = csvhandle.csvhandle()
